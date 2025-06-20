dadafb0e94194032deae24fe9da410b9ae032a36
Sanyasoso <aleksandrmopr@gmail.com>
2025-06-20 12:09:03 +0300

    new functions

--- Main file.py
+++ Main file.py
@@ -1,6 +1,9 @@
 import pygame
 
+pygame.init()
+
 screen = pygame.display.set_mode((700, 500))
+clock = pygame.time.Clock()
 
 idle_animation = [
     pygame.image.load('Sprites/Animation/Idle_anim/Playersprite_idle1.png'),
@@ -10,9 +13,22 @@ idle_animation = [
     pygame.image.load('Sprites/Animation/Idle_anim/Playersprite_idle5.png'),
 ]
 
+Left_idle_animation = [
+    pygame.transform.flip(pygame.image.load('Sprites/Animation/Idle_anim/Playersprite_idle1.png'), True, False),
+    pygame.transform.flip(pygame.image.load('Sprites/Animation/Idle_anim/Playersprite_idle2.png'), True, False),
+    pygame.transform.flip(pygame.image.load('Sprites/Animation/Idle_anim/Playersprite_idle3.png'), True, False),
+    pygame.transform.flip(pygame.image.load('Sprites/Animation/Idle_anim/Playersprite_idle4.png'), True, False),
+    pygame.transform.flip(pygame.image.load('Sprites/Animation/Idle_anim/Playersprite_idle5.png'), True, False)
+]
 
-p_x = 10
+current_frame = 0
+animation_speed = 100  # Миллисекунды между кадрами (чем меньше, тем быстрее)
+last_update = pygame.time.get_ticks() # Время последнего обновления кадра
+
+left_dirrection = True
+p_x = 100
 p_y = 492
+
 
 frame = 0
 idle_run = True
@@ -21,19 +37,22 @@ while game_run:
 
     screen.blit(pygame.image.load("Sprites/Bg/phon.png"), (0,0))
 
-    if idle_run:
-        screen.blit(idle_animation[frame], (p_x,p_y))
-        if frame == 4:
-            frame = 0
-        else:
-            frame += 1
+    now = pygame.time.get_ticks()
+    if now - last_update > animation_speed:
+        last_update = now
+        current_frame = (current_frame + 1) % len(idle_animation)
+
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_d]:
+        left_dirrection = False
         p_x += 3
     elif keys[pygame.K_a]:
         p_x -= 3
+        left_dirrection = True
+       
 
+    screen.blit(idle_animation[current_frame], (p_x, p_y))
 
     pygame.display.update()
 
